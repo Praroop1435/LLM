e6adcf3d085b6e93e06fa47bbe5080ac3e371669
Praroop1435 <anandpraroop@gmail.com>
2026-01-28 16:55:30 +0530

    Runnables and Chains

---
+++ langchain-runnables/simple_llm_app.py
@@ -0,0 +1,26 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize the LLM
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V3.2",
+    task="text-generation"
+) #type : ignore
+model = ChatHuggingFace(llm = llm)
+
+# Create a Prompt Template
+prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="Suggest a catchy blog title about {topic}."
+)
+
+# Format prompt
+formatted_prompt = prompt.format(topic="Cricket")
+
+# Call the LLM
+blog_title = model.invoke(formatted_prompt)
+
+print("Generated Blog Title:", blog_title)
